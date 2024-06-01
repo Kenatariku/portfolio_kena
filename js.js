2d96053f69ab0323891b8b5902e4546d83ad2627
Kenatariku <151837577+Kenatariku@users.noreply.github.com>
2024-06-01 21:25:30 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+let menuIcon = document.querySelector("#menu-icon');
+
+let navbar= document.querySelector('.navbar");
+
+let sections = document.querySelectorAll('section');
+
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+    sections.forEach(sec=>
+
+        let top = window.scrollY;
+
+        let offset = sec.offsetTop-150;
+
+        let height = sec.offsetHeight;
+
+        let id sec.getAttribute("id");
+
+        if(top >= offset && top < offset + height){
+          navLinks.forEach(links =>{
+            links.classtist.remove('active'); 
+            document.querySelector('header nav a [href*=' + id +]').classList.add ('active')
+          })
+        }
+})
+}
